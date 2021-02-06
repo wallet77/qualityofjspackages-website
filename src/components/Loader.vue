42dbf73cf6876db5e7323de3f78e934433f3b549
wallet77 <wallet77@gmail.com>
2021-02-06 15:18:05 +0100

    Remove sass-loader + clean loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -63,113 +63,95 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-    #content {
-    margin: 0 auto;
-    padding-bottom: 50px;
-    width: 80%;
-    max-width: 978px;
-    }
-
-    h1 {
-    font-size: 40px;
-    }
-
-    /* The Loader */
-    #loader-overlay {
+<style scoped>
+/* The Loader */
+#loader-overlay {
     background-color: grey;
     opacity: 0.25;
     z-index: 9;
-    }
+}
 
-    #loader-wrapper, #loader-overlay {
+#loader-wrapper, #loader-overlay {
     position: absolute;
     z-index: 10;
     overflow: hidden;
+}
+
+#loader {
+    display: block;
+    position: relative;
+    margin: 0px 0 0 0px;
+    border-radius: 50%;
+    border: 3px solid transparent;
+    border-top-color: #16a085;
+    animation: spin 1.7s linear infinite;
+    z-index: 11;
+}
+#loader:before {
+    content: "";
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    right: 5px;
+    bottom: 5px;
+    border-radius: 50%;
+    border: 3px solid transparent;
+    border-top-color: #e74c3c;
+    animation: spin-reverse .6s linear infinite;
+}
+
+#loader:after {
+    content: "";
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    right: 15px;
+    bottom: 15px;
+    border-radius: 50%;
+    border: 3px solid transparent;
+    border-top-color: #f9c922;
+    animation: spin 1s linear infinite;
+}
+
+@keyframes spin-reverse {
+    0% {
+        -webkit-transform: rotate(0);
+        -moz-transform: rotate(0);
+        -ms-transform: rotate(0);
+        -o-transform: rotate(0);
+        transform: rotate(0);
     }
-
-    #loader {
-        display: block;
-        position: relative;
-        margin: 0px 0 0 0px;
-        border-radius: 50%;
-        border: 3px solid transparent;
-        border-top-color: #16a085;
-        animation: spin 1.7s linear infinite;
-        z-index: 11;
-
-        &:before {
-        content: "";
-        position: absolute;
-        top: 5px;
-        left: 5px;
-        right: 5px;
-        bottom: 5px;
-        border-radius: 50%;
-        border: 3px solid transparent;
-        border-top-color: #e74c3c;
-        animation: spin-reverse .6s linear infinite;
-        }
-
-        &:after {
-        content: "";
-        position: absolute;
-        top: 15px;
-        left: 15px;
-        right: 15px;
-        bottom: 15px;
-        border-radius: 50%;
-        border: 3px solid transparent;
-        border-top-color: #f9c922;
-        animation: spin 1s linear infinite;
-        }
+    100% {
+        -webkit-transform: rotate(-360deg);
+        -moz-transform: rotate(-360deg);
+        -ms-transform: rotate(-360deg);
+        -o-transform: rotate(-360deg);
+        transform: rotate(-360deg);
     }
+}
 
-    @keyframes spin-reverse {
-        0% {
-            -webkit-transform: rotate(0);
-            -moz-transform: rotate(0);
-            -ms-transform: rotate(0);
-            -o-transform: rotate(0);
-            transform: rotate(0);
-        }
-        100% {
-            -webkit-transform: rotate(-360deg);
-            -moz-transform: rotate(-360deg);
-            -ms-transform: rotate(-360deg);
-            -o-transform: rotate(-360deg);
-            transform: rotate(-360deg);
-        }
+@keyframes spin {
+    0% {
+        -webkit-transform: rotate(0);
+        -moz-transform: rotate(0);
+        -ms-transform: rotate(0);
+        -o-transform: rotate(0);
+        transform: rotate(0);
     }
+    100% {
+        -webkit-transform: rotate(360deg);
+        -moz-transform: rotate(360deg);
+        -ms-transform: rotate(360deg);
+        -o-transform: rotate(360deg);
+        transform: rotate(360deg);
+    }
+}
 
-    @keyframes spin {
-        0% {
-            -webkit-transform: rotate(0);
-            -moz-transform: rotate(0);
-            -ms-transform: rotate(0);
-            -o-transform: rotate(0);
-            transform: rotate(0);
-        }
-        100% {
-            -webkit-transform: rotate(360deg);
-            -moz-transform: rotate(360deg);
-            -ms-transform: rotate(360deg);
-            -o-transform: rotate(360deg);
-            transform: rotate(360deg);
-        }
-    }
-
-    #content {
-        width: 25%;
-        height: 25%;
-        border: 1px solid black;
-    }
-
-    .pending {
-        filter: progid:DXImageTransform.Microsoft.Blur(PixelRadius='3');
-        -webkit-filter: url(#blur-filter);
-        filter: url(#blur-filter);
-        -webkit-filter: blur(3px);
-        filter: blur(3px);
-    }
+.pending {
+    filter: progid:DXImageTransform.Microsoft.Blur(PixelRadius='3');
+    -webkit-filter: url(#blur-filter);
+    filter: url(#blur-filter);
+    -webkit-filter: blur(3px);
+    filter: blur(3px);
+}
 </style>
