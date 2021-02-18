76bdf702bead00ed959d7cf089fcd91b240d374e
wallet77 <wallet77@gmail.com>
2021-02-18 20:58:00 +0100

    fix: use base_url to access data

--- src/services/reports.js
+++ src/services/reports.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 export default class ReportsService {
   static async fetch() {
-    const res = await axios.get("data.json")
+    const res = await axios.get(`${process.env.BASE_URL}data.json`)
     return res.data
   }
 }
