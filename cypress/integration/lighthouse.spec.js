23a730bd8c2bb91be93ab59cca7528b98c6eed94
wallet77 <wallet77@gmail.com>
2021-04-23 06:28:10 +0200

    ci: setup tests for execution on CI

--- cypress/integration/lighthouse.spec.js
+++ cypress/integration/lighthouse.spec.js
@@ -1,7 +1,10 @@
-context('Test website perf', () => {
-    it('should pass the audits', () => {
+context('Test website quality', () => {
+    beforeEach(() => {
         cy.visit('/')
         cy.get('.section').should('be.visible')
+    })
+
+    it('should pass the lighthouse audits', () => {
         cy.lighthouse(
             null,
             {
@@ -9,6 +12,11 @@ context('Test website perf', () => {
                 screenEmulation: { disabled: true }
             }
         )
+    })
+
+    it('should pass the pa11y audits', () => {
+        cy.visit('/')
+        cy.get('.section').should('be.visible')
         cy.pa11y()
     })
 })
